834b396d77c4725661f65363ebd070dd0200b299
Ronojoy Adhikari <rjoy@IMSCs-MacBook-Air.local>
2014-04-12 17:55:04 +0530

    missing pybisp.py added

---
+++ pybisp.py
@@ -0,0 +1,25 @@
+from __future__ import division
+
+'''
+pybisp : a library for simulation and inference on stochastic processes
+@author : Ronojoy Adhikari
+@author : Vanitha Kevin
+'''
+
+import numpy as np
+import pandas as pd
+
+
+def ReadDataFrame(filename):
+	'''
+	Loads a csv file into a pandas data frame
+	'''
+	return pd.read_csv(filename)
+
+def WienerInference(data):
+	pass
+
+def OrnsteinUhlenbeckInference(data):
+	pass	
+
+
