bc48af2935e9c16d07a5ed93e7683fab2e0abbf7
Vanitha <vanithakvn@gmail.com>
2014-04-17 13:29:09 +0530

    Update and rename wienerclass.py to Wienerclass.py

--- Wienerclass.py
+++ Wienerclass.py
@@ -24,8 +24,7 @@ f.write(str(t))                             # write the output as a string
 f.close()                                   # Close the file:
 data = pb.ReadDataFrame('diffusion.csv')    # read diffusion time series data from csv 
 data = pd.read_csv('diffusion.csv')         # Load csv into pandas dataframe
-df = Data
-#
+##
 def WienerInference(data):
     return -dx2/(2*D*dt) - np.log(D) -0.5*(N-1)*np.log(2*D*np.pi*(dt))
             
