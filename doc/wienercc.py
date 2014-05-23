db286e7165778c93d32064d5d6075b928df20c27
Vanitha <vanithakvn@gmail.com>
2014-05-24 03:09:32 +0530

    Update wienercc.py

--- doc/wienercc.py
+++ doc/wienercc.py
@@ -9,25 +9,10 @@ from __future__ import division
 from pandas import DataFrame, read_csv
 from pybisp import ReadDataFrame
 from pybisp import WienerInference, OrnsteinUhlenbeckInference
-#
+import matplotlib.pyplot as plt
 
-class estimation:                                                              # class object : estimate
-   
-    
-      def __init__(self,posterior,variance):                                   # Constructor for the class : Initialisation : Method 
-          self.posterior, self.variance = posterior,variance
-        
-                             
-          x = estimation()                                                         # Instantiate the class :  
-          print x.WienerInference(self,posterior,variance)
-        
-        
-                    
-      def WienerInference(self,posterior,variance):                  # Function returns the posterior and variance :
-          return estimation(posterior,variance)
-            
 
-N = 60
+N = 1028
 t = arange(N) # sampling times
 dw = np.random.randn(N) # Wiener increments
 x = np.cumsum(dw) # Brownian path
@@ -39,11 +24,34 @@ dx2 = sum((x[1:] - x[:-1])**2)
 D = np.linspace(0.8,1.2, 1028)
 
 
+class estimation():                                              # class object : estimate
+    
+     
+    def __init__(self,posterior,variance):                     # Constructor for the class : Initialisation :
+        self.posterior = posterior
+        self.variance = variance
+        
+    
+                
+    def WienerInference(data):                                   # Function returns the posterior and variance :
+        return estimation(posterior,variance)
+    
+    
+posterior = -dx2/(2*D*dt) - np.log(D) -0.5*(N-1)*np.log(2*D*np.pi*(dt))
+variance  = -2*(N+1)*(dt)**2/dx    
 
-posterior = -dx2/(2*D*dt) - np.log(D) -0.5*(N-1)*np.log(2*D*np.pi*(dt)) # Assign posterior and variance :
-variance = -2*(N+1)*(dt)**2/dx
+       
+# 
+x = estimation(posterior,variance)                                  # Instantiate the class :
+print x.posterior
+print x.variance
 
-print posterior
+filename = 'diffusion.csv'   
+data = pb.ReadDataFrame(filename)
+#print data
+estimate = pb.WienerInference(data)
+print estimate
+
 ###
 INCOMPLETE CODE : TO BE REWRITTEN :
 #########
