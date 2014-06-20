16d9ca973b7aaffba3a9b27b735b5c20400c0e83
Vanitha <vanithakvn@gmail.com>
2014-06-20 16:21:49 +0530

    Update wiener.py

--- doc/wiener.py
+++ doc/wiener.py
@@ -15,6 +15,8 @@ def ReadDataFrame(filename):
 def WienerInference(data1):
     return -dx2/(2*D*dt) - np.log(D) -0.5*(N-1)*np.log(2*D*np.pi*(dt))
 #
+def OrnsteinUhlenbeckInference(data2):
+    return((N/2)*np.log(2*sigma**2*dt)-(1/sigma**2)*(x2-x1*exp(-gamma*dt))**2/2*dt+np.log(1/gamma))
 #
 filename = 'diffusion.csv'                    # Give the filename(e.g.'diffusion.csv' here) in this line :
 data = pb.ReadDataFrame(filename)             # read diffusion time series data from csv :
@@ -39,13 +41,22 @@ D = np.linspace(0.8,1.2, 1028)
 data1 = {"N":N,"dx2": dx2, "D":D,"dt":dt, "dx":dx}      # Initialise the parameters values here :
 print data1
 #
+data2 = {"sigma":sigma,"gamma":gamma,"dt":dt,"N":N,"x2": x2, "x1":x1}
+print data2
 #Estimate the parameters:(This error part to be rewritten)
 #
 estimate = pb.WienerInference(data1)
 print estimate
 #
+plot(D, WienerInference(data1),'r',lw=2)
+#plot(gamma, OrnsteinUhlenbeckInference(data2),'r',lw=2)
+#
+xlabel('D')
+ylabel('P(D)')
+grid('on')
 
-
+print dx2/(N*dt)
+##############################3
 
 
 
