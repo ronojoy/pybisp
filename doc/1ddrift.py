f15761305672d4df329ab45cce11f35eb455f944
Vanitha <vanithakvn@gmail.com>
2014-06-27 15:31:59 +0530

    Update 1ddrift.py

--- doc/1ddrift.py
+++ doc/1ddrift.py
@@ -1,16 +1,43 @@
 # Estimating approximate likelihood inference for the parameters of the one dimensional drift-diffusion process:
 # This work is under progress:
 #
-import numpy as np
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import pylab as pl
-from __future__ import division
-#
-#
-The one dimensional drift-diffusion process is described by the following stochastic differential equation: 
-dx=adt+2D−−−√dW for which the Fokker-Planck equation is ∂P∂t=D∂2P∂x2−a∂P∂x.
-a is a drift and W is a wiener process which models white noise.
-The transition probability density is given by P(x,t)=12πDt√exp−(x−at)22Dt.
-The exact solution is the Gaussian with mean and variance given by ⟨x(t)⟩=∫−∞∞xp(x,t)dx = at, ⟨x2⟩−⟨x⟩2 = Dt so that the transition probability density is given by P(x,t)=12πDt√exp−(x−at)22Dt. 
-The parameters are a=⟨x(t)⟩t, D=⟨x2⟩−⟨x⟩2t. The parameter in this process is D and a.
+{
+ "metadata": {
+  "name": "drift"
+ },
+ "nbformat": 3,
+ "nbformat_minor": 0,
+ "worksheets": [
+  {
+   "cells": [
+    {
+     "cell_type": "code",
+     "collapsed": false,
+     "input": "import numpy as np\nimport matplotlib as mpl\nimport matplotlib.pyplot as plt\nimport pylab as pl\nfrom __future__ import division",
+     "language": "python",
+     "metadata": {},
+     "outputs": [],
+     "prompt_number": 5
+    },
+    {
+     "cell_type": "code",
+     "collapsed": false,
+     "input": "The one dimensional drift-diffusion process is described by the following stochastic differential equation:\n$dx = adt + \\sqrt{2D}dW$ for which the Fokker-Planck equation is $\\frac{\\partial P}{\\partial t}  = D \\frac{\\partial^{2}P}{\\partial x^{2}}\n-a\\frac{\\partial P}{\\partial x}$. a is a drift and W is a wiener process which models white noise. The transition probability density is\ngiven by $P(x,t) = \\frac{1}{\\sqrt{2\\pi Dt}}\\exp{-\\frac{(x-at)^{2}}{2Dt}}$. The exact solution is the Gaussian with mean and variance given\nby $\\langle x(t)\\rangle = \\int\\limits_{-\\infty}^\\infty x p(x,t)dx$ = at, $\\langle x^{2} \\rangle - \\langle x \\rangle ^2$ = Dt so that the\ntransition probability density is given by $P(x,t) = \\frac{1}{\\sqrt{2\\pi Dt}}\\exp{-\\frac{(x-at)^{2}}{2Dt}}$.\nThe parameters are $a = \\frac{\\langle x(t)\\rangle}{t}$, $ D = \\frac{\\langle x^{2} \\rangle - \\langle x \\rangle ^2}{t}$. The parameter in this \nprocess is  D and a.\n",
+     "language": "python",
+     "metadata": {},
+     "outputs": []
+    },
+    {
+     "cell_type": "code",
+     "collapsed": false,
+     "input": "The one dimensional drift-diffusion process and its transition probability density",
+     "language": "python",
+     "metadata": {},
+     "outputs": [],
+     "prompt_number": 5
+    }
+   ],
+   "metadata": {}
+  }
+ ]
+}
