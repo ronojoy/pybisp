f689e404c20e30030a6f5b391834b488c26d7c8d
Ronojoy Adhikari <ronojoy.adhikari@gmail.com>
2015-03-16 18:03:35 +0530

    added OU logprob

--- logprob.py
+++ logprob.py
@@ -1,10 +1,19 @@
 from __future__ import division
 import numpy as np
 
-def wiener(D, dx2, dt, N):
+def wiener(D, data, dt):
+      N = np.len(data)
+      dx = np.diff(data)
+      dx2 = np.sum(dx*dx)
       return -dx2/(2*D*dt) - np.log(D) -0.5*(N-1)*np.log(2*D*np.pi*(dt))
 
-def oup():
-    ## Sudipta will add the logprob calculation here
-    pass
+def oup(theta, data, dt, a):
+      N = np.len(data)
+      a2 = a*a
+      gamma, D = theta[0], theta[1]
+      dx = data[1:] - np.exp(-gamma)*data[-1:]
+      dx2 = np.sum(dx*dx)
+      B = 1 - np.exp(-2*gamma)
+      return 0.5*(N-1)*np.log(gamma/(2*np.pi*D*B*a2)) - gamma *dx2/(2*D*I*a2)
+
 
