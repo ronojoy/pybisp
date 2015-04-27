d84013a044e954462172aa03aafa333348b52ae7
Sudipta K Bera <sudiptabera08@gmail.com>
2015-04-27 15:34:20 +0530

    added more files

--- pybisp.py
+++ pybisp.py
@@ -55,9 +55,18 @@ class OrnsteinUhlenbeckProcess:
       def setPrior(self, prior):
         '''priors for parameters'''
 
-      def logProb(self,theta):
+      def logProb(self,theta1,theta2):
         '''analytical expression for the logarithm of the posterior'''
-
+        N = np.size(self.path[:, 1])
+        x= self.path[:,0]
+        t = self.path[:,1]
+        dt = t[1] -t[0]
+        mu = theta1
+        sigma = theta2
+        dx2 = np.sum((x[1:]-x[:-1]*np.exp(-mu*dt))**2)
+        I = 1-np.exp(-2*mu*dt)
+        s1 = 0.5*(N-1)*np.log(mu/(np.pi*sigma*sigma*I))-mu*dx2/(sigma*sigma*I)
+        return s1
       def mapEstimate(self, method):
         '''analytical or numerical maximum aposteriori estimate of parameters'''
 
@@ -69,3 +78,33 @@ class OrnsteinUhlenbeckProcess:
 
       def plotLogProb(self, extent):
         '''plot of the posterior probability'''
+        #th = extent*0     # initializing x to be zeros of same size as extent
+        th = np.zeros((np.size(extent[:,0]),np.size(extent[:,1])))
+        for l in range(np.size(extent[:,0])):
+                for m in range(np.size(extent[:,1])):
+            #'''call logProb method and update x'''
+                     th[l,m] = self.logProb(extent[l,0],extent[m,1])
+        #np.savetxt('output.txt', th, delimiter=" ")
+        #print np.shape(th)
+        plt.clf()
+        mu=extent[:,0]
+        sigma = extent[:,1]
+        mu, sigma = np.meshgrid(mu,sigma) 
+        np.savetxt('output.txt', th, delimiter=" ")
+        np.savetxt('mu.txt', mu, delimiter=" ")
+        np.savetxt('sigma.txt', sigma, delimiter=" ")  
+        plt.axis([mu.min(), mu.max(),sigma.min(),sigma.max()])
+        plt.pcolor(mu, sigma,th)
+	plt.colorbar()
+	plt.hold('on')
+        [i, j] = np.where(th == np.max(th))
+        plt.plot(mu[i,j],sigma[i,j],'gs')
+	plt.axis('tight')
+        plt.xlabel(r'$\mu$')
+        plt.ylabel('sigma')
+        plt.show()
+	
+        print i, j
+
+
+
