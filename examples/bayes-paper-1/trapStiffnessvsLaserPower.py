02992acaf6cbf2b837f5070c9dae50f3d598f84c
Rajesh Singh <rajeshrinet@gmail.com>
2016-09-29 22:22:32 +0530

    minor changes

--- examples/bayes-paper-1/trapStiffnessvsLaserPower.py
+++ examples/bayes-paper-1/trapStiffnessvsLaserPower.py
@@ -1,4 +1,4 @@
-# Code to plots the laser power vs trap stiffness 
+# Code to plots the laser power vs trap stiffness estimated from Bayes I, II and PSD 
 #
 #
 
@@ -18,13 +18,13 @@ errPSD  = [0.02*1.30, 0.05*2.40, 0.06*3.99, 0.08*4.25, 0.09*4.40, 0.10*4.67, 0.1
 ebayes1 = [0.06*1.10, 0.01*2.23, 0.02*3.88, 0.02*4.16, 0.02*4.48, 0.03*4.83, 0.03*6.01]
 ebayes2 = [0.06*1.10, 0.05*2.23, 0.05*3.88, 0.02*4.16, 0.02*4.48,0.02*4.83, 0.03*6.01]
 
-#Bayes1
+##Bayes1
 plt.plot(laserP, bayes1, 's', color="g", markersize=6, mec='g', label='Bayes $I$' )
 plt.errorbar(laserP, bayes1, ebayes2, fmt='none', ecolor="g" )
 slopeInter = np.polyfit(laserP, bayes1, 1)
 p = np.poly1d(slopeInter)
 plt.plot(laserP, p(laserP), '-', color="g" )
-#
+
 ##Bayes2
 plt.plot(laserP, bayes2, 'o', mfc='none', markersize=12.5, mec="#A60628", mew=1.5, color="#A60628", label='Bayes $II$')
 plt.errorbar(laserP, bayes1, ebayes1, fmt='none', ecolor="#A60628" )
@@ -38,7 +38,6 @@ plt.errorbar(laserP, PSD, errPSD, fmt='none', ecolor="#348ABD"  )
 slopeInter = np.polyfit(laserP, PSD, 1)
 p = np.poly1d(slopeInter)
 plt.plot(laserP, p(laserP), '--', color="#348ABD" )
-print slopeInter
 
 plt.xlabel('Laser power (mW)', fontsize=24)
 plt.ylabel('k(pN/$\mu$m) ', fontsize=24);
@@ -51,5 +50,5 @@ plt.legend(loc=2, fontsize=16)
 plt.text(24, 1.24, r"Slope from Bayes $I$ and $II$=0.139" "\n" "Slope from PSD=0.135", bbox={'facecolor':'white', 'alpha':0.5, 'pad':4}, fontsize=16)
 
 
-plt.savefig('errorBayes1.jpg', dpi=256)
+#plt.savefig('errorBayes1.jpg', dpi=256)
 plt.show()
--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ from distutils.core import setup
 
 
 setup(
-    name='pybisp',
+    name='PyBISP',
     version='0.0.1',
     url='https://github.com/ronojoy/pybisp',
     author='Rajesh Singh and R. Adhikari',
     author_email='rsingh@imsc.res.in and  rjoy.imsc.res.in',
     license='MIT',
-    description='python library for Bayesian inference of stochastic processes',
-    long_description='to be added',
-    platforms='tested on LINUX',
+    description='Python library for Bayesian Inference of Stochastic Processes',
+    long_description='Python library for performing Bayesian Inference of Stochastic Processes and plotting the data and results',
+    platforms='Tested on Linux and Mac',
     libraries=[],
     packages=['pybisp'],
 )
